ddb8a9fc236ce602fc6d008b73f9316b9cae146e
Forrest Lyman <forrestswork@gmail.com>
2023-11-02 12:44:36 -0700

    removes data client library

--- projects/data-clients/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of data-clients
- */
-
-export * from './lib/data-clients.service';
-export * from './lib/data-clients.component';
-export * from './lib/data-clients.module';
